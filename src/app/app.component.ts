65b512601de5ffcbad6c75bb7d2e34255aeaa9b8
Rocío <rocio.sirvent@finologee.com>
2021-06-16 21:50:51 +0200

    add programming logic and refactor the template
    - Remove data-key attribute from keyboard keys, since it's not needed
    - add event listener on the document level, to listen to any key press
    - replace data-key attribute from the audio elements for the keyCode, as it's easier to capture
    - add programming logic to capture the keypress of a key and match it with its corresponding sound

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-drum-kit';
+
+  onKeydown(event) {
+        const audioElements = document.querySelectorAll('audio');
+        audioElements.forEach(audio => {
+          //   take the event keycode and see if it matches with any audio sound
+          if(audio.getAttribute('data-keyCode') === event.code) {
+            audio.play();
+          }
+      });
+  }
 }
